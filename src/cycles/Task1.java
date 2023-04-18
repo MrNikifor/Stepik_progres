78c436e382c6557ca565f4e232e4d58dfe34649a
osian <oisa.nikifor@gmail.com>
2023-04-19 02:46:33 +0300

    For

--- src/cycles/Task1.java
+++ src/cycles/Task1.java
@@ -1,2 +1,29 @@
-package cycles;public class Task1 {
+package cycles;
+
+import java.util.Scanner;
+
+/*
+Пользователь вводит с клавиатуры натуральное число n. Затем вводит n целых чисел.
+Необходимо найти и вывести на экран их сумму.
+Sample Input:
+3
+7
+2
+9
+Sample Output:
+18
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = n;
+        int sum = 0;
+        while (a > 0) {
+            a--;
+            int x = sc.nextInt();
+            sum = sum + x;
+        }
+        System.out.print(sum);
+    }
 }
--- src/cycles/Task2.java
+++ src/cycles/Task2.java
@@ -1,2 +1,30 @@
-package cycles;public class Task2 {
+package cycles;
+
+import java.util.Scanner;
+
+/*
+Пользователь вводит с клавиатуры натуральное число n. Затем вводит n целых чисел. Необходимо найти и вывести на экран количество чётных.
+Sample Input:
+3
+7
+2
+9
+Sample Output:
+1
+*/
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = n;
+        int sum = 0;
+        while (a > 0) {
+            a--;
+            int x = sc.nextInt();
+            if (x % 2 == 0) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
 }
